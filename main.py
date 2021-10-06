dc74ef6a1abc182a3990e603e66d4e05f72e0749
Shubham Shrivastava <shubhamshekharshrivastava@gmail.com>
2021-10-07 01:40:06 +0530

    Fromatted main file

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="http://example.com",
-        client_id="",
-        client_secret="",
+        client_id="e0b19c7e6bbc416f80d316779cbae96f",
+        client_secret="e37bfc376ae74e758d1033ff744ab356",
         show_dialog=True,
         cache_path="token.txt"
     )
 )
-date = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format.")
+date = input("Which year do you want to travel to? Type the date in YYYY-MM-DD format: ")
 scrap = web_scraping.Scrapping(date, sp)
 scrap.find_songs()
 scrap.get_song_uri()
